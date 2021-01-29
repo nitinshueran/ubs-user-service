58bb9bf1083e3e41b009bead4ecb5823c78f31b8
nitin <nitin@nitins-mbp.fritz.box>
2021-01-29 17:29:33 +0100

    MYSQL auto configurations and pom

---
+++ src/main/java/com/example/demo/UbsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbsUserServiceApplication.class, args);
+	}
+
+}
